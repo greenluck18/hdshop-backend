0d4db9096c492d5354f21cce9c17ac6c18ee3648
Halyna Mazur <greenluck3.14159@gmail.com>
2024-06-24 22:33:16 +0300

    ch db.js

--- db.js
+++ db.js
@@ -11,6 +11,38 @@ const {
     DB_PORT
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`);
+// const sequelize = new Sequelize(`postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`);
+
+
+const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you connect to the PostgreSQL database with SSL
+      rejectUnauthorized: false // You can adjust this based on your needs
+    },
+    connectTimeout: 30000 // Optional: set a connection timeout (in ms)
+  },
+  logging: false, // Optional: disable logging; default is console.log
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+
+// Test the connection
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+testConnection();
 
 export { sequelize }
