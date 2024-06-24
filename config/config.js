d4f24b6b7d38af72c477abd1146ba2891bcdff4a
Halyna Mazur <greenluck3.14159@gmail.com>
2024-06-24 21:19:48 +0300

    change config file

---
+++ config/config.js
@@ -0,0 +1,22 @@
+require('dotenv').config(); // Load environment variables from .env file
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres'
+  }
+};
