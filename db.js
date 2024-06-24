dfdd19da76ad30ae2fd006365e9abc89021a74ee
Halyna Mazur <greenluck3.14159@gmail.com>
2024-06-24 22:36:26 +0300

    ch

--- db.js
+++ db.js
@@ -3,46 +3,42 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const {
-    DB_USERNAME,
-    DB_PASSWORD,
-    DB_DATABASE,
-    DB_HOST,
-    DB_PORT
-} = process.env;
+const { DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST, DB_PORT } = process.env;
 
 // const sequelize = new Sequelize(`postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`);
 
-
-const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
+const sequelize = new Sequelize({
+  database: DB_DATABASE,
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
   host: DB_HOST,
-  dialect: 'postgres',
+  dialect: "postgres",
   dialectOptions: {
     ssl: {
       require: true, // This will help you connect to the PostgreSQL database with SSL
-      rejectUnauthorized: false // You can adjust this based on your needs
+      rejectUnauthorized: false, // You can adjust this based on your needs
     },
-    connectTimeout: 30000 // Optional: set a connection timeout (in ms)
+    connectTimeout: 30000, // Optional: set a connection timeout (in ms)
   },
   logging: false, // Optional: disable logging; default is console.log
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 });
 
 // Test the connection
 async function testConnection() {
   try {
     await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
+    console.log("Connection has been established successfully.");
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error("Unable to connect to the database:", error);
   }
 }
 
 testConnection();
 
-export { sequelize }
+export { sequelize };
