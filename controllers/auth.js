b654261f428e35f56d593e600e8c1f1c14068a31
Halyna Mazur <greenluck3.14159@gmail.com>
2024-12-18 00:46:28 +0200

    cc

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,8 @@
-// create and get the Item 
-import { Users } from '../models/users.js';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import crypto from 'crypto';
+// create and get the Item
+import { Users } from "../models/users.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 /**
  * Items
@@ -11,31 +11,27 @@ import crypto from 'crypto';
 
 class AuthController {
   /**
-    * ping - pong
-    * @param {Request} req
-    * @param {Response} res
-    */
+   * ping - pong
+   * @param {Request} req
+   * @param {Response} res
+   */
   constructor() {
     this.privateKey = null;
   }
 
   async generatePrivateKey() {
     const buf = crypto.randomBytes(64);
-    this.privateKey = buf.toString('hex');
+    this.privateKey = buf.toString("hex");
   }
   async createUser(req, res) {
     try {
-      const {
-        login,
-        password,
-        first_name,
-        last_name,
-        email
-      } = req.body;
-      const existingItem = await Users.findOne({ where: { login: login?.trim() } });
-      
+      const { login, password, first_name, last_name, email } = req.body;
+      const existingItem = await Users.findOne({
+        where: { login: login?.trim() },
+      });
+
       if (existingItem) {
-        res.status(404).json({ error: 'Error user already exist' });
+        res.status(404).json({ error: "Error user already exist" });
         return;
       } else {
         try {
@@ -45,15 +41,13 @@ class AuthController {
             password: bcrypt.hashSync(password, salt),
             first_name,
             last_name,
-            email
+            email,
           });
-          res.status(201).json({ userId: user.dataValues.id});
-        }
-        catch (error) {
+          res.status(201).json({ userId: user.dataValues.id });
+        } catch (error) {
           res.status(505);
           res.json(err);
         }
-        
       }
     } catch (err) {
       res.status(500);
@@ -62,37 +56,40 @@ class AuthController {
   }
   async login(req, res) {
     try {
-      const {
-        login,
-        password
-      } = req.body;
+      const { login, password } = req.body;
       const user = await Users.findOne({ where: { login: login } });
 
       if (user?.dataValues) {
-        const isMatch = await bcrypt.compare(password, user.dataValues.password);
-        
+        const isMatch = await bcrypt.compare(
+          password,
+          user.dataValues.password
+        );
+
         if (isMatch) {
           if (!this.privateKey) {
             // Generate private key for the first user
-            console.log('GENERATE_PR_KEY');
+            console.log("GENERATE_PR_KEY");
             await this.generatePrivateKey();
           }
           const accessToken = jwt.sign(
             {
-              "id": user.dataValues.id,
-              "email": user.dataValues.email,
-              "first_name": user.dataValues.first_name,
-              "expiresAt": new Date().getTime() + (24 * 60 * 60 * 1000)
-            }, this.privateKey);
+              id: user.dataValues.id,
+              email: user.dataValues.email,
+              first_name: user.dataValues.first_name,
+              expiresAt: new Date().getTime() + 24 * 60 * 60 * 1000,
+            },
+            this.privateKey
+          );
 
           console.log(accessToken);
-          res.status(200).json({ token: accessToken, userId: user.dataValues.id});
-
+          res
+            .status(200)
+            .json({ token: accessToken, userId: user.dataValues.id });
         } else {
-          res.status(404).json({ error: 'Error unauthorized' });
+          res.status(404).json({ error: "Error unauthorized" });
         }
       } else {
-        res.status(404).json({ error: 'Error unauthorized' });
+        res.status(404).json({ error: "Error unauthorized" });
       }
     } catch (err) {
       res.status(500);
@@ -100,22 +97,22 @@ class AuthController {
       res.json(err);
     }
   }
+
   async authenticateToken(req, res, next) {
-  
-    const token = req.header('Authorization');
-    console.log('TOKEN', req.header('Authorization'));
-    console.log(this.privateKey)
+    const token = req.header("Authorization");
     if (!token) {
-      return res.status(401).json({ message: 'Unauthorized - Missing Token. Login first' });
+      return res
+        .status(401)
+        .json({ message: "Unauthorized - Missing Token. Login first" });
     }
 
     jwt.verify(token, this.privateKey, (err, user) => {
       if (err) {
-        return res.status(403).json({ message: 'Forbidden - Invalid Token' });
+        return res.status(403).json({ message: "Forbidden - Invalid Token" });
       }
       req.user = user;
       if (new Date().getTime() > user.expiresAt) {
-        return res.status(403).json({ message: 'Forbidden - Token Expired' });
+        return res.status(403).json({ message: "Forbidden - Token Expired" });
       }
       next();
     });
@@ -123,7 +120,7 @@ class AuthController {
   async allUsers(req, res) {
     try {
       const users = await Users.findAll({
-        attributes: ['login', 'email'] 
+        attributes: ["login", "email"],
       });
       res.status(200).json(users);
     } catch (err) {
